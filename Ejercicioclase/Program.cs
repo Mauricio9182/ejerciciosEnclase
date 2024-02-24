aa5a02f041f1f5868585af39ba1ec8ad501b7d7b
mauricio9182 <pablocarrilopb0199@gmail.com>
2024-02-24 09:54:31 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicioclase/Program.cs
@@ -0,0 +1,50 @@
+﻿/*using System.Runtime.Serialization.Formatters;
+
+int x = 1;
+while (x <= 500)
+{
+    Console.WriteLine("{0}- ", x);
+    x++;
+}
+Console.WriteLine("el loop finaliza");
+
+int f;
+for(f=1;f<=100;f++) 
+{
+    Console.WriteLine("{0}-" f);
+}
+Console.ReadKey();*/
+
+/*using System.Runtime.Serialization.Formatters;
+
+int x;
+for (x = 1; x <= 20; x++) ;
+{
+    x = x % 2;
+    if (x == 0)
+    {
+        Console.WriteLine("x");
+    }
+
+}*/
+
+
+
+/*for (int i = 0; i <= 20; i++) 
+{
+    if (i % 2 == 0) 
+    {
+        Console.WriteLine(i); 
+    }
+}*/
+
+
+
+
+int suma = 0;
+for (int i = 1; i <= 10; i++);
+{
+    
+}
+
+Console.WriteLine("La suma de los primeros 10 números naturales es: " + suma);
